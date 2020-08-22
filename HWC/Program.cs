4b0febfc66136f921fd0226a003c74b43f958045
unknown <kajack99@gmail.com>
2020-08-21 20:00:16 -0500

    new

---
+++ HWC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorldCovidStyle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Are you staying safe?");
+            var userResponse = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you.");
+
+            Console.WriteLine("What's your age?");
+            userResponse = Console.ReadLine();
+            Console.WriteLine("That's old! I'm younger. Haha!");
+
+            Console.WriteLine("What did you do today?");
+            userResponse = Console.ReadLine();
+            Console.WriteLine("Oh, that's all... Ever think about watching less tv?");
+            userResponse = Console.ReadLine();
+            Console.WriteLine("You should.");
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
